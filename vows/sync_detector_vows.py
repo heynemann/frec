c2e3b5994dc9d91fd25cc510b194c7509710b02e
Bernardo Heynemann <heynemann@gmail.com>
2012-06-15 15:54:33 -0300

    commenting out invalid vows

--- vows/sync_detector_vows.py
+++ vows/sync_detector_vows.py
@@ -12,23 +12,24 @@ from pyvows import Vows, expect
 
 import frec.detectors.sync_detector as detector
 
-@Vows.batch
-class SyncDetector(Vows.Context):
-    class ShouldReturnDetectedItemsInPicture(Vows.Context):
-        def topic(self):
+#@Vows.batch
+#class SyncDetector(Vows.Context):
+    #class ShouldReturnDetectedItemsInPicture(Vows.Context):
+        #def topic(self):
+            #det = detector()
+            #det.detect(img)
+            #return loader.load(context, 'some/invalid/path', callback)
 
-            return loader.load(context, 'some/invalid/path', callback)
+        #def should_be_none(self, topic):
+            #expect(topic.args[0]).to_be_null()
 
-        def should_be_none(self, topic):
-            expect(topic.args[0]).to_be_null()
+    #class ShouldReturnContentIfProper(Vows.Context):
+        #@Vows.async_topic
+        #def topic(self, callback):
+            #context = ctx.Context(config=cfg.Config())
+            #return loader.load(context, 'test.txt', callback)
 
-    class ShouldReturnContentIfProper(Vows.Context):
-        @Vows.async_topic
-        def topic(self, callback):
-            context = ctx.Context(config=cfg.Config())
-            return loader.load(context, 'test.txt', callback)
-
-        def should_be_none(self, topic):
-            expect(topic.args[0]).to_equal('test_data')
+        #def should_be_none(self, topic):
+            #expect(topic.args[0]).to_equal('test_data')
 
 
