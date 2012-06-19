69014502bfdcb8f07785f7a2e2e1bda298290cf4
Bernardo Heynemann <heynemann@gmail.com>
2012-06-19 16:09:20 -0300

    Tan Triggs processing vows

---
+++ vows/tan_triggs_preprocessing_vows.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# frec face recognition service
+# https://github.com/heynemann/frec
+
+# licensed under the mit license:
+# http://www.opensource.org/licenses/mit-license
+# copyright (c) 2012 bernardo heynemann heynemann@gmail.com
+
+import os.path as path
+from collections import defaultdict
+
+from pyvows import Vows, expect
+
+import frec.recognizers.pre_processing.tan_triggs as proc
+import frec.image as image
+
+ROOT_PATH = path.abspath(path.join(path.dirname(__file__), 'orl_faces'))
+image_path = lambda person, picture: path.join(ROOT_PATH, 's%d' % person, '%d.pgm' % picture)
+
+test_data = []
+for person in range(1, 41):
+    for picture in range(1, 10):
+        test_data.append((person, picture, image_path(person, picture)))
+
+cache = {}
+def _read(path):
+    if path in cache: return cache[path]
+    with open(path, 'rb') as f:
+        contents = f.read()
+        cache[path] = contents
+        return contents
+
+
+@Vows.batch
+class TanTriggsPreProcessing(Vows.Context):
+    def topic(self):
+        return proc.TanTriggsPreProcessing()
+
+    def should_be_instance_of(self, topic):
+        expect(topic).to_be_instance_of(proc.TanTriggsPreProcessing)
+
+    class WhenPreprocessingImage(Vows.Context):
+        def topic(self, pre_processor):
+            images = defaultdict(list)
+            for data in test_data:
+                person, picture, file_path = data
+                images[person].append(image.Image.create_from_buffer(_read(file_path)).to_array())
+
+            people = []
+            images_list = []
+            for person, images in images.iteritems():
+                people.append(person)
+                images_list.append(images)
+
+            return pre_processor.compute(images_list, people)
+
+        def should_not_be_empty(self, topic):
+            expect(topic).not_to_be_null()
+            expect(topic).not_to_be_an_error()
+
+
