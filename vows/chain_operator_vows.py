47c2eb2fa52f3f822c851c76c351c3ea5b513fa0
Bernardo Heynemann <heynemann@gmail.com>
2012-06-23 11:39:19 -0300

    Autopep8 chain_operator_vows.py

--- vows/chain_operator_vows.py
+++ vows/chain_operator_vows.py
@@ -17,7 +17,8 @@ import frec.recognizers.operators as op
 import frec.image as image
 
 ROOT_PATH = path.abspath(path.join(path.dirname(__file__), 'orl_faces'))
-image_path = lambda person, picture: path.join(ROOT_PATH, 's%d' % person, '%d.pgm' % picture)
+image_path = lambda person, picture: path.join(
+    ROOT_PATH, 's%d' % person, '%d.pgm' % picture)
 
 test_data = []
 for person in range(1, 41):
@@ -25,8 +26,11 @@ for person in range(1, 41):
         test_data.append((person, picture, image_path(person, picture)))
 
 cache = {}
+
+
 def _read(path):
-    if path in cache: return cache[path]
+    if path in cache:
+        return cache[path]
     with open(path, 'rb') as f:
         contents = f.read()
         cache[path] = contents
@@ -39,17 +43,20 @@ class TanTriggsPreProcessing(Vows.Context):
         return op.ChainOperator()
 
     def should_be_instance_of(self, topic):
-        expect(topic).to_be_instance_of(proc.TanTriggsPreProcessing)
+        expect(topic).to_be_instance_of(op.ChainOperator)
 
     def should_have_proper_representation(self, topic):
-        expect(str(topic)).to_equal('TanTriggsPreprocessing (alpha=0.100,tau=10.000,gamma=0.200,sigma0=1.000,sigma1=2.000)')
+        expect(str(topic)).to_equal('ChainOperator()')
 
     class WhenPreprocessingImage(Vows.Context):
         def topic(self, pre_processor):
             images = defaultdict(list)
             for data in test_data:
                 person, picture, file_path = data
-                images[person].append(image.Image.create_from_buffer(_read(file_path)).to_array())
+                img = image.Image.create_from_buffer(_read(file_path))
+                images[person].append(
+                    img.to_array()
+                )
 
             people = []
             images_list = []
@@ -62,5 +69,3 @@ class TanTriggsPreProcessing(Vows.Context):
         def should_not_be_empty(self, topic):
             expect(topic).not_to_be_null()
             expect(topic).not_to_be_an_error()
-
-
