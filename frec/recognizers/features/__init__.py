fb5fba927da24833a0039b9ee3672432deac991b
Bernardo Heynemann <heynemann@gmail.com>
2012-06-23 11:30:29 -0300

    Autopep8 features __init__.py

--- frec/recognizers/features/__init__.py
+++ frec/recognizers/features/__init__.py
@@ -8,16 +8,21 @@
 # http://www.opensource.org/licenses/mit-license
 # copyright (c) 2012 bernardo heynemann heynemann@gmail.com
 
-# code adapted from facerec's feature module (https://github.com/bytefish/facerec)
+# code adapted from facerec's feature module
+# (https://github.com/bytefish/facerec)
+
 
 class AbstractFeature(object):
 
     def compute(self, x, y):
-        raise NotImplementedError("Every AbstractFeature must implement the compute method.")
+        raise NotImplementedError(
+            "Every AbstractFeature must implement the compute method."
+        )
 
     def extract(self, x, y):
-        raise NotImplementedError("Every AbstractFeature must implement the extract method.")
+        raise NotImplementedError(
+            "Every AbstractFeature must implement the extract method."
+        )
 
     def __repr__(self):
         return "AbstractFeature"
-
