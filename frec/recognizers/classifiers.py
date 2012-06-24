f3d36a39e0f830f9867d210445e0f05b5aba3d9e
Bernardo Heynemann <heynemann@gmail.com>
2012-06-24 13:40:40 -0300

    Abstract classifier done

---
+++ frec/recognizers/classifiers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# frec face recognition service
+# https://github.com/heynemann/frec
+
+# licensed under the mit license:
+# http://www.opensource.org/licenses/mit-license
+# copyright (c) 2012 bernardo heynemann heynemann@gmail.com
+
+# code adapted from facerec's classifier module
+# (https://github.com/bytefish/facerec)
+
+class AbstractClassifier(object):
+    def compute(self, x, y):
+        msg = "Every AbstractClassifier must implement the compute method."
+        raise NotImplementedError(msg)
+
+    def predict(self, x):
+        msg = "Every AbstractClassifier must implement the predict method."
+        raise NotImplementedError(msg)
+
+
---
+++ vows/classifier_vows.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# frec face recognition service
+# https://github.com/heynemann/frec
+
+# licensed under the mit license:
+# http://www.opensource.org/licenses/mit-license
+# copyright (c) 2012 bernardo heynemann heynemann@gmail.com
+
+from pyvows import Vows, expect
+
+import frec.recognizers.classifiers as cl
+
+@Vows.batch
+class AbstractClassifier(Vows.Context):
+    def topic(self):
+        return cl.AbstractClassifier()
+
+    def should_be_instance_of(self, topic):
+        expect(topic).to_be_instance_of(cl.AbstractClassifier)
+
+    class OnCompute(Vows.Context):
+        def topic(self, classifier):
+            return classifier.compute(1, 2)
+
+        def should_be_an_error(self, topic):
+            expect(topic).to_be_an_error()
+            expect(topic).to_be_an_error_like(NotImplementedError)
+
+
+    class OnPredict(Vows.Context):
+        def topic(self, classifier):
+            return classifier.predict(1)
+
+        def should_be_an_error(self, topic):
+            expect(topic).to_be_an_error()
+            expect(topic).to_be_an_error_like(NotImplementedError)
+
