437f941b7b38927c369bf54e1d6c3f93fb6caadb
Bernardo Heynemann <heynemann@gmail.com>
2012-06-24 13:47:39 -0300

    addin distance

---
+++ frec/recognizers/distance.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# frec face recognition service
+# https://github.com/heynemann/frec
+
+# licensed under the mit license:
+# http://www.opensource.org/licenses/mit-license
+# copyright (c) 2012 bernardo heynemann heynemann@gmail.com
+
+# code adapted from facerec's distance module
+# (https://github.com/bytefish/facerec)
+
+import numpy as np
+
+class AbstractDistance(object):
+    def __init__(self, name):
+        self._name = name
+
+    def __call__(self,p,q):
+        raise NotImplementedError("Every AbstractDistance must implement the __call__ method.")
+
+    @property
+    def name(self):
+        return self._name
+
+    def __repr__(self):
+        return self._name
+
+class EuclideanDistance(AbstractDistance):
+    def __init__(self):
+        AbstractDistance.__init__(self,"EuclideanDistance")
+
+    def __call__(self, p, q):
+        p = np.asarray(p).flatten()
+        q = np.asarray(q).flatten()
+        return np.sqrt(np.sum(np.power((p-q),2)))
+
+
